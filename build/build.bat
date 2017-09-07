c6171b56c703441c415bfe2ec1c6e7efadcd45d1
申行 <binary2015nov@outlook.com>
2017-09-07 11:08:00 +0800

    update dll

--- build/build.bat
+++ build/build.bat
@@ -1,4 +1,4 @@
-SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe"
+SET MSBUILD="C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
 
 REM %MSBUILD% build-core.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=41
 REM %MSBUILD% build.proj /target:TeamCityBuild;NuGetPack /property:Configuration=Release;PatchVersion=9
@@ -17,4 +17,4 @@ COPY App_Data\db.sqlite ..\..\tests\TestWebHost\App_Data
 RMDIR ..\ServiceStack.Admin\ss_admin /s /q
 XCOPY /E wwwroot ..\ServiceStack.Admin\ss_admin\
 
-msbuild /p:Configuration=Release ..\src\ServiceStack.Admin.sln
+%msbuild% /p:Configuration=Release ..\src\ServiceStack.Admin.sln
--- build/copy.bat
+++ build/copy.bat
@@ -3,3 +3,4 @@ SET BUILD=Release
 
 COPY ..\src\ServiceStack.Admin\bin\%BUILD%\net45\ServiceStack.Admin.* ..\..\ServiceStack\lib\net45
 COPY ..\src\ServiceStack.Admin\bin\%BUILD%\netstandard1.6\ServiceStack.Admin.* ..\..\ServiceStack\lib\netstandard1.6
+pause
